752a266935c26c10ab2cd0376f2637752af12a51
hex1n <nixehh@163.com>
2021-06-30 18:26:32 +0800

    skin to mysql

---
+++ src/main/java/com/dlwlrma/flink/api/sink/SinkTest4_JDBC.java
@@ -0,0 +1,68 @@
+package com.dlwlrma.flink.api.sink;
+
+import com.dlwlrma.flink.pojo.SensorReading;
+import org.apache.flink.configuration.Configuration;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.functions.sink.RichSinkFunction;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+
+/**
+ * @author hex1n
+ * @date 2021/6/30 18:08
+ * @description
+ */
+public class SinkTest4_JDBC {
+    public static void main(String[] args) throws Exception {
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        env.setParallelism(1);
+
+        //从文件读取数据
+        DataStream<String> inputStream = env.readTextFile("D:\\learn\\flink\\flink-demo\\src\\main\\resources\\sensorReading.txt");
+        DataStream<SensorReading> dataStream = inputStream.map(value -> {
+            String[] fields = value.split(",");
+            return new SensorReading(fields[0], new Long(fields[1]), new Double(fields[2]));
+        });
+        dataStream.addSink(new MyJdbcSink());
+        env.execute();
+    }
+
+    // 实现自定义的SinkFunction
+    public static class MyJdbcSink extends RichSinkFunction<SensorReading> {
+        // 声明连接和预编译
+        Connection connection = null;
+        PreparedStatement insertStmt = null;
+        PreparedStatement updateStmt = null;
+
+        @Override
+        public void open(Configuration parameters) throws Exception {
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/test", "root", "123456");
+            insertStmt = connection.prepareStatement("insert into t_test(id,temp) values (?,?)");
+            updateStmt = connection.prepareStatement("update t_test set temp=? where id=?");
+        }
+
+        // 每来一条数据,调用连接,执行sql
+        @Override
+        public void invoke(SensorReading value, Context context) throws Exception {
+            // 直接执行更新语句,如果没有更新那么就插入
+            updateStmt.setDouble(1, value.getTemperature());
+            updateStmt.setString(2, value.getId());
+            updateStmt.execute();
+            if (updateStmt.getUpdateCount() == 0) {
+                insertStmt.setString(1, value.getId());
+                insertStmt.setDouble(2, value.getTemperature());
+                insertStmt.execute();
+            }
+        }
+
+        @Override
+        public void close() throws Exception {
+            insertStmt.close();
+            updateStmt.close();
+            connection.close();
+        }
+    }
+}
